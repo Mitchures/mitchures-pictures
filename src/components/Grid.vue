e385d40f28ccde078732c2173714cf321ba3cf30
Mitchell Hollander <MitchellSH@live.com>
2019-10-17 21:10:33 -0500

    now reading all images from a dir and displaying them via webpack require.context

---
+++ src/components/Grid.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="grid">
+            <div :key="key" v-for="(img, key) in images" class="col-xs-12">
+                <img :src="img"  alt=""/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "Grid",
+      data () {
+        return {
+          images: {}
+        }
+      },
+      mounted () {
+        this.importAll(require.context("../assets/images/photography/", true, /\.jpg$/));
+      },
+      methods: {
+        importAll(r) {
+          let imgs = {};
+          r.keys().forEach(key => (imgs[key] = r(key)));
+          this.images = imgs;
+        }
+      }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
+    <Grid/>
   </div>
 </template>
 
 <script>
+  import Grid from "../components/Grid";
   export default {
     name: 'home',
     components: {
+      Grid
     }
   }
 </script>
