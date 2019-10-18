30f016d569d8d4dbc28d6bf81f128ba10cc7aa92
Mitchell Hollander <MitchellSH@live.com>
2019-10-17 20:32:10 -0500

    first commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav>
+        <div class="container">
+            <div class="grid">
+                <div class="col-xs-12 col-sm-6">
+                    <figure class="media">
+                        <div class="logo">
+                            <a href="/">
+                                <img src="../assets/images/m-icon-outline.png"  alt="Mitchures"/>
+                            </a>
+                        </div>
+                        <figcaption class="logo-content">
+                            <span>Mitchures Pictures</span>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+      name: "Nav"
+    }
+</script>
